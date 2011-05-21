2f2df93a4d308daf2067f60a2b84a35ae6482b24
Abraham Williams <4braham@gmail.com>
2011-05-20 19:14:52 -0700

    Move Storify link to the end of actions links.

--- storify-twitter.js
+++ storify-twitter.js
@@ -1,46 +1,47 @@
-waitForjQuery();
+// Stay in the Storify namespace (SFY).
+var SFY = SFY || {};
 
-function waitForjQuery() {
-  if (typeof jQuery == "undefined") {
-    setTimeout(waitForjQuery, 10000);
-  } else {
-    initStorify();
-  }
-}
+// We use a newer version of jQuery than Twitter does.
+$.noConflict();
 
-function initStorify() {
+jQuery(document).ready(function($) {
+  var baseUrl = SFY.baseUrl
+    , appname = SFY.appname
+    , strictEncodeURIComponent = SFY.strictEncodeURIComponent;
+
   function setTarget(permalink) {
-    return 'http://storify.com/import?permalink='+encodeURIComponent(permalink);
+    return baseUrl+'/import?appname='+appname+'&permalink='+strictEncodeURIComponent(permalink)+'&text=&title=';
   }
 
+  // Storify #newtwitter .tweet(s)
   $('#page-container').delegate('.tweet', 'mouseover', function() {
-    var permalinkAttr = "data-storify-permalink";
-    if ( !$(this).attr(permalinkAttr) || !$(this).find('.storify-action') ) {
-      var permalink = 'http://twitter.com/' + $(this).attr('data-screen-name')
-                    + '/status/' + $(this).attr('data-tweet-id');
-      $(this).attr(permalinkAttr, permalink);
+    var elem = $(this)
+      , permalinkAttr = 'data-storify-permalink';
+
+    if ( elem.find('.storify-action').length === 0 || !elem.attr(permalinkAttr) ) {
+      var permalink = 'http://twitter.com/' + elem.attr('data-screen-name')
+                    + '/status/' + elem.attr('data-tweet-id');
+      elem.attr(permalinkAttr, permalink);
       var target = setTarget(permalink);
-      var storifyAction = $('<a href="' + target + '" class="storify-action" '
-                        + '><span><i></i><b>Storify</b></span></a>');
-      $(this).find('.tweet-actions').prepend(storifyAction);
-      storifyAction.click(function(e) {
-        event.preventDefault();
-        var w = 460,
-            h = 580,
-            sh = screen.height,
-            sw = screen.width,
-            top = 0;
-        var left = Math.round((sw/2)-(w/2));
-        if(sh>h){
-          top=Math.round((sh/2)-(h/2))
-        }
-        var popup = window.open(target,"storify_this","left="+left+",top="+top+",width="+w+",height="+h+",personalbar=0,toolbar=0,scrollbars=1,resizable=1");
-        if (popup) {
-          popup.focus();
-        } else {
-          window.location.href=target;
-        }
-      });
+      var storifyAction = $('<a/>', {
+          href: target,
+          class: 'storify-action',
+          html: '<span><i></i><b>Storify</b></span>',
+          click: function(e) {
+            e.preventDefault();
+            var windowSettings
+              , w = 460
+              , h = 580
+              , sh = screen.height
+              , sw = screen.width
+              , top = 0
+              , left = Math.round((sw/2)-(w/2));
+            if (sh > h) top = Math.round((sh/2)-(h/2));
+            windowSettings = 'left='+left+',top='+top+',width='+w+',height='+h+',personalbar=0,toolbar=0,scrollbars=1,resizable=1';
+            window.open(target, 'storify_this', windowSettings);
+          }
+        });
+      elem.find('.tweet-actions').append(storifyAction);
     }
   });
-}
+});
