99ce0d69fed2bbfabd4428dd52b17eede9678aa1
Azat Mardanov <azat@storify.com>
2013-06-24 10:43:34 -0700

    commented disqus module

--- js/load.js
+++ js/load.js
@@ -24,24 +24,35 @@ var service = getService(window.location.href)
   , onStorify = window.location.hostname.match(/storify\.com/);
 
 var embeds = {
-  "fyre": ".fyre-widget"
+  'fyre': '.fyre-widget, #livefyre_comments'//,
+  // 'disqus': '#disqus_thread'
 }
 
-//AM: sometimes Storify is used on embeds, life fyre
-if (!service) {
-  for (var key in embeds) {
-    if ($(embeds[key])) {
-      service = key;
-      break;
-    }
-  }
-}
-// console.log(service);
+
+// console.log(typeof service);
 
 $('body').attr('storify-loaded', 'true');
 
 sfy.loadCSS('css/storify-common.css');
 
 if (!onStorify && service && service !== 'facebook') {
+  // console.log(sfy.fn);
   sfy.fn[service]();
+}
+
+//AM: sometimes Storify is used on embeds, life fyre
+if (!service) {
+  $(document).ready(function(){
+    for (var key in embeds) {
+      // console.log($(embeds[key]));
+      if ($(embeds[key]).length > 0) {
+        service = key;
+        break;
+      }
+    }    
+    if (service) {
+      // console.log(service)
+      sfy.fn[service]();
+    }
+  })
 }
